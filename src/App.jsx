0fbdfc1562df156968073c714c56d0b9583a5c4e
Alireza Fatemi <fa1381138100@gmail.com>
2023-09-09 11:15:00 +0330

    Developed by react-router-dom @6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Switch , Route ,Redirect } from "react-router-dom";
+import { Routes , Route ,Navigate } from "react-router-dom";
 
 import SignUp from './assets/component/SignUp';
 import Login from './assets/component/Login';
@@ -8,11 +8,11 @@ import Login from './assets/component/Login';
 const App = () => {
   return (
     <div className='app' >
-      <Switch>
-        <Route path='/signup' component={SignUp} />
-        <Route path='/login' component={Login} />
-        <Redirect from='/' to='/signup' />
-      </Switch>
+      <Routes>
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Navigate to='/signup' />} />
+      </Routes>
     </div>
   );
 };
